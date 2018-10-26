a588891b272d9055e225d8449ffa33a7c747aeba
owen <owen@J-deb.home.lan>
2018-10-27 00:20:14 +0100

    mess lots of mess

--- src/main.c
+++ src/main.c
@@ -6,49 +6,58 @@
 
 void main(void)
 {
-  unsigned char inbyte, chkey;
+  unsigned char inbyte, chkey, lch, cch;
+  chl=0;
+  chc=0;
 
-  //quick initalise serial port
-
+  // quick initalise serial port
   rs232_params(RS_BAUD_9600|RS_STOP_1|RS_BITS_8,RS_PAR_NONE);
   rs232_init();
 
-  //main loop
-  printf("Terminal ready...");
+  // Clean up the screen
+  zx_border(INK_BLACK);
+  clg();
+  zx_colour(PAPER_BLACK|INK_WHITE);
 
+
+  // main loop
+  printf("Terminal ready...");
   while(1)
   {
-    //delay to slow the loop giving the key press time
-
-    //in_Pause(20);
-
-    //read keyboard and send
-    for (int i=0; i<20; i++)
-    {
-      chkey = getk();
-      if (chkey != NULL)
-      {
-        rs232_put(chkey);
-      }
-    }
-    //chkey = NULL;  //clear the key buffer?
-
-    //read in any data at the port
-
-    if(rs232_get(&inbyte)!=RS_ERR_NO_DATA)
+    if(rs232_get(&inbyte)!=RS_ERR_NO_DATA)  //any incoming data capture and print
     {
       // filter input here
 
 
+      cch = inbyte;
+
       // default output everything
       //putch(inbyte);
-      //printf("%c",inbyte);
-      fputc_cons(inbyte);
+      printf("%c",inbyte);
+      //fputc_cons(inbyte);
+
+      //  quick keyboard check if we are reading alot so we can interupt
+
+      for (int i=0; i<1; i++)
+      {
+        chkey = getk();
+        if (chkey != NULL)
+        {
+          rs232_put(chkey);
+        }
+      }
+
     }
-    else
+    else //no incoming data check keyboard
     {
-      //in_Wait(1);
-      in_Pause(10);  //slow the code when no data incoming
+      for (int i=0; i<20; i++)
+      {
+        chkey = getk();
+        if (chkey != NULL)
+        {
+          rs232_put(chkey);
+        }
+      }
     }
 
 
