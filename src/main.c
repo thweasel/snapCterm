69113bd25360b2371b63dc7054cbfcf4f07c662e
owen <owen@J-deb.home.lan>
2018-10-28 13:12:37 +0000

    House keeping,
    Changed the filters to reset the inbyte not the last in byte.  Its 
    pointless when you set the lastbyte after using the inbyte...  Really 
    nothing needs to happen, could scan the keyboard or something else tbh.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+// Compiler line (not using make)
+// zcc +zx -clib=ansi -pragma-define:ansicolumns=80 -lndos -lm -lrs232plus -create-app -subtype=wav main.c
+
 #include <conio.h>
 #include <stdio.h>
 #include <spectrum.h>
@@ -6,10 +9,8 @@
 
 void main(void)
 {
-  unsigned char inbyte, chkey, chl, chc;
-
-  chl=0;
-  chc=0;
+  unsigned char inbyte, chkey, lastbyte, chl, chc;
+  lastbyte=0;
 
   // quick initalise serial port
   rs232_params(RS_BAUD_9600|RS_STOP_1|RS_BITS_8,RS_PAR_NONE);
@@ -18,8 +19,7 @@ void main(void)
   // Clean up the screen
   zx_border(INK_BLACK);
   clg();
-  zx_colour(PAPER_BLACK|INK_WHITE);
-
+  zx_colour(PAPER_WHITE|INK_BLACK);
 
   // main loop
   printf("Terminal ready...");
@@ -29,25 +29,29 @@ void main(void)
     {
       // filter input here
 
-
-      chc = inbyte;
-
-      if (chl==0xff && chc==0xff)  //catch Telnet IAC
+      if (lastbyte==0xff && inbyte==0xff)  //catch Telnet IAC drop it
       {
-        //drop the character
-        chl=0;  //reset last character chc will over write
+        inbyte=0;  //reset inbyte
       }
-      else
+      else if (inbyte==0x0d)  // Catch Carriage Return
+      {  // could be left out as 0x0d triggers a new line if printed to screen
+        printf("\n");
+        inbyte=0;  //reset inbyte
+      }
+      else if (inbyte==0x0a)  // Catch Line Feed
       {
-        // default output everything
-        //putch(inbyte);
-        printf("%c",inbyte);
-        //fputc_cons(inbyte);
+        inbyte=0;  //reset inbyte
       }
+      else  // default output the character to the screen
+      {
+        fputc_cons(inbyte);
+      }
+
+      lastbyte=inbyte;
 
       //  quick keyboard check if we are reading alot so we can interupt
 
-      for (int i=0; i<1; i++)
+      for (int i=5; i>0; i--)
       {
         chkey = getk();
         if (chkey != NULL)
@@ -59,7 +63,7 @@ void main(void)
     }
     else //no incoming data check keyboard
     {
-      for (int i=0; i<20; i++)
+      for (int i=20; i>0; i--)
       {
         chkey = getk();
         if (chkey != NULL)
@@ -68,8 +72,6 @@ void main(void)
         }
       }
     }
-
-
 
   }
 
