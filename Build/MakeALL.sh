dbecbadf47837aebab0edf4f59e338482186ffa3
thweasel <thweasel@gmail.com>
2019-03-31 12:13:22 +0100

    Fixed filename in the build process

--- Build/MakeALL.sh
+++ Build/MakeALL.sh
@@ -7,7 +7,7 @@ mkdir ../snapCterm-if1
 cp Makefile.snapCterm-if1 ../snapCterm-if1/Makefile.snapCterm-if1
 cd ../snapCterm-if1
 make -f ../snapCterm-if1/Makefile.snapCterm-if1
-cp ../snapCterm-if1/sCt-IF1.tap ../Build/snapCterm-IF1.tap
+cp ../snapCterm-if1/sCtIF1.tap ../Build/snapCterm-IF1.tap
 cd ../Build
 
 #Build 128K Plus
@@ -16,7 +16,7 @@ mkdir ../snapCterm-p
 cp Makefile.snapCterm-p ../snapCterm-p/Makefile.snapCterm-p
 cd ../snapCterm-p
 make -f ../snapCterm-p/Makefile.snapCterm-p
-cp ../snapCterm-p/sCt-p.tap ../Build/snapCterm-plus.tap
+cp ../snapCterm-p/sCtp.tap ../Build/snapCterm-plus.tap
 cd ../Build
 
 #Build Plus3 disk
@@ -25,5 +25,5 @@ mkdir ../snapCterm-p3
 cp Makefile.snapCterm-p3 ../snapCterm-p3/Makefile.snapCterm-p3
 cd ../snapCterm-p3
 make -f ../snapCterm-p3/Makefile.snapCterm-p3
-cp ../snapCterm-p3/sCt-p3.dsk ../Build/snapCterm-plus3.dsk
+cp ../snapCterm-p3/sCtp3.dsk ../Build/snapCterm-plus3.dsk
 cd ../Build
