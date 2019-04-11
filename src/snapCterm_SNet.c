8cdf980133cb2a9e37dabeed5e4389506c11761e
thweasel <thweasel@gmail.com>
2019-04-12 00:41:36 +0100

    Little clean up port moved to var

--- src/snapCterm_SNet.c
+++ src/snapCterm_SNet.c
@@ -9,17 +9,18 @@
 
 uint_fast8_t io_initialized=0;
 
-int sockfd, bytes, pfd;
+int sockfd, pfd, host_port;
 struct sockaddr_in remoteaddr;
 struct hostent *he;
 char host_name[64];
 
 void CommsInit(void)
 {
-    strcpy(host_name,"amstrad.simulant.uk");
+    strcpy(host_name,"amstrad.simulant.uk");    //  will need to be removed menu option
+    host_port = 464;                            //  will need to be removed menu option
     he=gethostbyname(host_name);
     sockfd=socket(AF_INET,SOCK_STREAM,0);
-    remoteaddr.sin_port=htons(464);
+    remoteaddr.sin_port=htons(host_port);
     remoteaddr.sin_addr.s_addr=he->h_addr;
     connect(sockfd,&remoteaddr,sizeof(struct sockaddr_in));    
     io_initialized=1;
@@ -30,8 +31,9 @@ void RX(void)
     *RXAttr = PAPER_RED;      //Indicate RX started    
     pfd=poll_fd(sockfd);
     if (pfd & POLLIN)
-    {      
-      rxbytes=recv(sockfd,rxdata,1,0);      
+    {
+        *RXAttr = PAPER_RED + BRIGHT;
+        rxbytes=recv(sockfd,rxdata,rxdata_Size,0);
     }
     *KBAttr = PAPER_BLACK;
 }
