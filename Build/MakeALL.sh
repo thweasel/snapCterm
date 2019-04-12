017d7d3623ba5c3e0fb6bb6a7c9a1b751708aaeb
thweasel <thweasel@gmail.com>
2019-04-12 21:25:52 +0100

    Build process update

--- Build/MakeALL.sh
+++ Build/MakeALL.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+Ver="Beta2.0"
+
 #Build RS232 IF1
 pwd
 rm ../src/*.o
@@ -8,18 +10,18 @@ mkdir ../snapCterm-if1-rs232
 cp Makefile.snapCterm-if1-rs232 ../snapCterm-if1-rs232/Makefile.snapCterm-if1-rs232
 cd ../snapCterm-if1-rs232
 make -f ../snapCterm-if1-rs232/Makefile.snapCterm-if1-rs232
-cp ../snapCterm-if1-rs232/sCtIF1rs.tap ../Build/snapCterm-IF1-rs232.tap
+cp ../snapCterm-if1-rs232/sCtIF1rs.tap ../Build/snapCterm-IF1-rs232_$Ver.tap
 cd ../Build
 
-#Build 128K RS232 Plus
-#rm ../src/*.o
-#rm -r ../snapCterm-p-rs232
-#mkdir ../snapCterm-p-rs232
-#cp Makefile.snapCterm-p-rs232 ../snapCterm-p-rs232/Makefile.snapCterm-p-rs232
-#cd ../snapCterm-p-rs232
-#make -f ../snapCterm-p-rs232/Makefile.snapCterm-p-rs232
-#cp ../snapCterm-p-rs232/sCtprs.tap ../Build/snapCterm-plus-rs232.tap
-#cd ../Build
+#Build 128K RS232 (Plus)
+rm ../src/*.o
+rm -r ../snapCterm-p-rs232
+mkdir ../snapCterm-p-rs232
+cp Makefile.snapCterm-p-rs232 ../snapCterm-p-rs232/Makefile.snapCterm-p-rs232
+cd ../snapCterm-p-rs232
+make -f ../snapCterm-p-rs232/Makefile.snapCterm-p-rs232
+cp ../snapCterm-p-rs232/sCtprs.tap ../Build/snapCterm-plus-rs232_$Ver.tap
+cd ../Build
 
 #Build 128K SNET Plus
 rm ../src/*.o
@@ -28,19 +30,21 @@ mkdir ../snapCterm-p-snet
 cp Makefile.snapCterm-p-snet ../snapCterm-p-snet/Makefile.snapCterm-p-snet
 cd ../snapCterm-p-snet
 make -f ../snapCterm-p-snet/Makefile.snapCterm-p-snet
-cp ../snapCterm-p-snet/sCtpsn.tap ../Build/snapCterm-plus-snet.tap
+cp ../snapCterm-p-snet/sCtpsn.tap ../Build/snapCterm-plus-snet_$Ver.tap
 cd ../Build
 
 #Build Plus3 RS232 disk
-#rm -r ../snapCterm-p3-rs232
-#mkdir ../snapCterm-p3-rs232
-#cp Makefile.snapCterm-p3-rs232 ../snapCterm-p3-rs232/Makefile.snapCterm-p3-rs232
-#cd ../snapCterm-p3-rs232
-#make -f ../snapCterm-p3-rs232/Makefile.snapCterm-p3-rs232
-#cp ../snapCterm-p3-rs232/sCtp3rs.dsk ../Build/snapCterm-plus3-rs232.dsk
-#cd ../Build
+rm ../src/*.o
+rm -r ../snapCterm-p3-rs232
+mkdir ../snapCterm-p3-rs232
+cp Makefile.snapCterm-p3-rs232 ../snapCterm-p3-rs232/Makefile.snapCterm-p3-rs232
+cd ../snapCterm-p3-rs232
+make -f ../snapCterm-p3-rs232/Makefile.snapCterm-p3-rs232
+cp ../snapCterm-p3-rs232/sCtp3rs.dsk ../Build/snapCterm-plus3-rs232_$Ver.dsk
+cd ../Build
 
 #Build Plus3 SNET disk  -- Impossible configuration at this time
+#rm ../src/*.o
 #rm -r ../snapCterm-p3-snet
 #mkdir ../snapCterm-p3-snet
 #cp Makefile.snapCterm-p3-snet ../snapCterm-p3-snet/Makefile.snapCterm-p3-snet
