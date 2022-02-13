45a1a1600b83dab791f903298ddc1f7ace820669
nen2408 <nen2408@gmail.com>
2022-02-13 21:50:47 +0000

    Split the title screen out for 80 and 40 col vers.

--- src/main.c
+++ src/main.c
@@ -72,6 +72,7 @@ _oemascii:
 #endasm
 #endif
 
+#ifdef __80col__
 void title(void)
 { 
   char titlescroll = 24;
@@ -115,7 +116,51 @@ void title(void)
     newline_attr();
   }while(--titlescroll!=0);
 }
+#endif
 
+#ifdef __40col__
+void title(void)
+{ 
+  char titlescroll = 24;
+  
+  for (uint8_t z=0;z<10;++z) {DrawCursor();  in_Pause(50);}
+  printf("\033[32;40m");
+  printf("[\373].40 columns  [\373].ASCII oem set  \n[\373].ANSI  [\373].colour clash   \n");
+  for (uint8_t z=0;z<10;++z) {DrawCursor();  in_Pause(100);}  
+  printf("Terminal ready... ");
+  for (uint8_t z=0;z<10;++z) {DrawCursor();  in_Pause(100);}
+  printf("\07"); in_Pause(50);
+  ClearCursor();
+  printf("\033[37;40m");
+  printf("\n\033[1m");
+  //      ----------==========----------==========
+  printf("\n");
+  printf("\n");
+  printf("snapCterm 40 column edition\n");
+  printf("\n");
+  printf("\n");
+  printf("\n");
+  printf("\033[1m\033[31;40m");
+  gotoxy(20-(sizeof(edition)+sizeof(version_num)-16/2),wherey());
+  printf("! - %s %s - !\n\n\n",edition,version_num);
+  printf("\033[1m\033[33;40m");
+  printf("    BY: Owen Reynolds 2018                      \n");
+  printf("CREDIT: Thomas Cherryhomes @IRATA.ONLINE       \n\n");
+  printf("\033[1m\033[37;40m");
+  printf("Built using Z88DK - C compiler for Z80s\n");
+  printf("\033[1m\033[34;40m");
+  printf("Facebook: Z88DK ZX Spectrum user group -\n\n\033[1m\033[37;40m");
+  printf("  -\\/\\/\\- ANY KEY TO CONTINUE -/\\/\\/-\033[37;40m");
+  in_WaitForKey();
+  Clear_Keyboard_buffer();
+
+  do
+  {
+    printf("\r");  
+    newline_attr();
+  }while(--titlescroll!=0);
+}
+#endif
 
 void Protocol(void)
 {
